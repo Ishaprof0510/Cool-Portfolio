89a4b7ae289923383e9111df52066b1819dac71a
AishaJS <isha.prof0510@gmail.com>
2025-02-25 10:52:37 -0700

    Generate HTML for the order summary

--- Practice JS /Amazon-project/data/cart.js
+++ Practice JS /Amazon-project/data/cart.js
@@ -1,4 +1,10 @@
-export const cart =[]; 
+export const cart =[{
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2
+}, {
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+}]; 
 
 export function addToCart(productId){
     let matchingItem;
@@ -10,11 +16,11 @@ export function addToCart(productId){
         })
   
         if (matchingItem) {
-          matchingItem.quanity += 1;
+          matchingItem.quantity += 1;
         } else {
           cart.push({
               productId: productId,
-              quanity: 1
+              quantity: 1
             });
         }
   }
